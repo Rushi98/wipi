d2e36ce41c270a55df62dbc9832a937c12059654
Omkar <omukanade@gmail.com>
2019-04-30 00:36:18 +0530

    cleared all tables in rpi.db and generated rpi_schema

--- Server/rpi_schema.sql
+++ Server/rpi_schema.sql
@@ -1,12 +1,5 @@
-CREATE TABLE `SCAN_DATA` (
-  `ts` datetime NOT NULL
-,  `ip_address` varchar(15) NOT NULL
-,  `mac_address` char(17) NOT NULL
-,  `device_manufacturer` varchar(100) DEFAULT NULL
-,  UNIQUE (`ts`)
-);
 CREATE TABLE ATTENDANCE_DATA (session DATETIME, mac_address char(17) NOT NULL, hits INTEGER, PRIMARY KEY (session, mac_address));
-CREATE TABLE IF NOT EXISTS "STUDENT_INFO"
+CREATE TABLE "STUDENT_INFO"
 (
 	mac_address char(17) not null
 		primary key,
